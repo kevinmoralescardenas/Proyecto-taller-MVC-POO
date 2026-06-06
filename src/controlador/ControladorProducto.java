5ee8d0714be22b332996e8ea46941600f8b1a8a8
anail <anail@CARLOSMORALES.mshome.net>
2026-06-06 15:35:21 -0500

    Guia #11 Restruccturacion del proyecto hacia MVC/ CARLOS

---
+++ src/controlador/ControladorProducto.java
@@ -0,0 +1,13 @@
+
+package controlador;
+import java.util.ArrayList;
+import modelo.Producto;
+
+public class ControladorProducto {
+    private ArrayList<Producto> inventario;
+    
+  public ControladorProducto() {
+    inventario = new ArrayList<>();
+    }
+}
+
---
+++ src/vista/VistaProducto.java
@@ -0,0 +1,17 @@
+
+package vista;
+
+public class VistaProducto {
+    public void mostrarMenu() {
+        System.out.println();
+        System.out.println("================================");
+        System.out.println(" SISTEMA DE INVENTARIO ");
+        System.out.println("================================");
+        System.out.println("1. Registrar producto");
+        System.out.println("2. Mostrar productos");
+        System.out.println("3. Buscar producto");
+        System.out.println("4. Modificar producto");
+        System.out.println("5. Eliminar producto");
+        System.out.println("6. Salir");
+    }
+}
