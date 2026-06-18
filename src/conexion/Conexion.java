8025b5b3e5ac8a92b39c676079d37c02b98dfc19
kevinmoralescardenas <kevinmoralesk123@gmail.com>
2026-06-18 13:58:40 -0500

    conexion con la base de datos, conexion.java y paquete conexion

---
+++ src/conexion/Conexion.java
@@ -0,0 +1,19 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    
+    //parametros de configuracion del servidor local
+    private static final String URL = "jdbc:postgresql://localhost:5432/proyectoinventario_db";
+    private static final String USUARIO = "postgres";
+    private static final String CLAVE = "admin123";
+    
+    public static Connection obtenerConexion() throws SQLException {
+        // DriverManager interactúa con el Driver .jar para levantar el socket de red
+        return DriverManager.getConnection(URL, USUARIO, CLAVE);
+    }
+    
+}
